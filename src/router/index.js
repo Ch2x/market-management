6afe1b02441750210172c6255eceaf518282edb4
Ch2x <1270427018@qq.com>
2019-12-30 23:04:04 +0800

    updated vue-router plugin

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import VueRouter from 'vue-router'
 import Market from '@/views/Market'
 import UserManagement from '@/views/UserManagement'
 import ProductManagement from '@/views/ProductManagement'
@@ -9,46 +8,48 @@ import SortManagement from '@/views/SortManagement'
 import OrderManagement from '@/views/OrderManagement'
 import Login from '@/views/Login'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Login',
-      component: Login,
-    },
-    {
-      path: '/market',
-      name: 'Market',
-      component: Market,
-      children: [
-        {
-          path: '/userManagement',
-          name: 'UserManagement',
-          component: UserManagement
-        },
-        {
-          path: '/productManagement',
-          name: 'ProductManagement',
-          component: ProductManagement
-        },
-        {
-          path: '/commentManagement',
-          name: 'CommentManagement',
-          component: CommentManagement
-        },
-        {
-          path: '/sortManagement',
-          name: 'SortManagement',
-          component: SortManagement
-        },
-        {
-          path: '/orderManagement',
-          name: 'OrderManagement',
-          component: OrderManagement
-        },
-      ]
-    }
-  ]
+const routes = [{
+    path: '/',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/market',
+    name: 'Market',
+    component: Market,
+    children: [{
+        path: '/userManagement',
+        name: 'UserManagement',
+        component: UserManagement
+      },
+      {
+        path: '/productManagement',
+        name: 'ProductManagement',
+        component: ProductManagement
+      },
+      {
+        path: '/commentManagement',
+        name: 'CommentManagement',
+        component: CommentManagement
+      },
+      {
+        path: '/sortManagement',
+        name: 'SortManagement',
+        component: SortManagement
+      },
+      {
+        path: '/orderManagement',
+        name: 'OrderManagement',
+        component: OrderManagement
+      },
+    ]
+  }
+]
+
+const router = new VueRouter({
+  routes
 })
+
+export default router
